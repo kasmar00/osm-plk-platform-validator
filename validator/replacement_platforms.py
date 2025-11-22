74a51289e508256d6b61d88d4d3e65aba45ac335
Marcin Kasznia <kasmar00@gmail.com>
2025-11-22 18:39:37 +0100

    New plk file for 2026, some replacements

--- validator/replacement_platforms.py
+++ validator/replacement_platforms.py
@@ -2,5 +2,7 @@ replacement_platforms = {
     ("Kraków Główny", "4", "8"): ("4", "6"),
     ("Kraków Piastów", "2", "2"): ("2", "102"),
     ("Kraków Piastów", "3", "1"): ("3", "101"),
-    ("Kraków Bronowice", "4", "2"): ("4", "2-L118")
+    ("Kraków Bronowice", "4", "2"): ("4", "2-L118"),
+    ("Świnoujście Warszów", "2", "1"): ("2", "2"),
+    ("Wysoka Kamieńska", "1", "9"): ("1", "7"),
 }
